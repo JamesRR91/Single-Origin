8517efa28b2c12b41d71f68301d559ac6086ecd6
JamesRR91 <jamesrusr@gmail.com>
2021-11-08 10:42:29 -0800

    grinder data in, next is recipe

--- authenticate-me/backend/db/seeders/20211108181002-grinder.js
+++ authenticate-me/backend/db/seeders/20211108181002-grinder.js
@@ -7,31 +7,57 @@ module.exports = {
         name: 'Capresso Infinity',
         price: 99,
         typeid: 'filter-coffee',
+        createdAt:new Date(),
+        updatedAt: new Date()
       },
       {
         name: 'Breville Smart Grinder Pro',
         price: 199,
         typeid: 'hybrid-grinder',
+        createdAt:new Date(),
+        updatedAt: new Date()
       },
       {
         name:'Baratza Encore Conical Burr Grinder',
         price:129,
         typeid: 'filter-coffee',
+        createdAt:new Date(),
+        updatedAt: new Date()
       },
       {
         name:'Eureka Mignon Notte',
         price:329,
         typeid: 'espresso',
+        createdAt:new Date(),
+        updatedAt: new Date()
       },
       {
         name:'Niche Zero Coffee Grinder',
         price:599,
         typeid: 'hybrid-grinder',
+        createdAt:new Date(),
+        updatedAt: new Date()
       },
       {
         name:'Fellow Ode Brew Grinder',
         price:299,
         typeid: 'filter-coffee',
+        createdAt:new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'La Marzocco Lux D Coffee Grinder',
+        price:975,
+        typeid: 'espresso',
+        createdAt:new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'Baratza Sette 270',
+        price:299,
+        typeid: 'espresso',
+        createdAt:new Date(),
+        updatedAt: new Date()
       },
     ], {});
   },
